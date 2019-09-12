55a496d4612c6071d67faa329514d1953bbc9e81
Galaxial <nzenker0028@gmail.com>
2019-09-12 19:42:57 -0400

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -25,7 +25,7 @@ module.exports = function(environment) {
 
     blog: {
       title: 'Noah Zenker',
-      description: 'Grand Technomancer',
+      description: '',
       coverImage: '/images/cover-custom.jpg',
 
       navigation: [{
